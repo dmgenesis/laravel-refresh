4ad1d15a698baab4b01b6e2fec060d6b02764ae3
Genesis del Mundo <dmgenesis@Genesiss-MacBook-Pro.local>
2020-12-19 17:25:50 +0800

    setup tailwind css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Laravel Refresh</title>
+        <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Posty</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body>
+    <body class="bg-gray-100">
+        <nav class="p-6 bg-white flex justify-between">
+            <ul class="flex items-center">
+                <li>
+                    <a href="#" class="p-3">Home</a>
+                </li>
+                <li>
+                    <a href="#" class="p-3">Dashboard</a>
+                </li>
+                <li>
+                    <a href="#" class="p-3">Post</a>
+                </li>
+            </ul>
+        </nav>
         @yield('content')
     <body>
 </html>
